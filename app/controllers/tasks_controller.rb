268b482cbbd73cfc8794272f68ce5b8178be5972
EC2 Default User <ec2-user@ip-172-31-21-194.ap-northeast-1.compute.internal>
2022-07-07 03:18:30 +0000

    C5完了 名称のタスク降順変更　7/7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   def index
      @user = User.find(params[:user_id])
-     @tasks = @user.tasks
+    # @tasks = @user.tasks
+     @tasks = @user.tasks.where(user_id: @user.id).order(created_at: :desc)
   end   
     
   def new
