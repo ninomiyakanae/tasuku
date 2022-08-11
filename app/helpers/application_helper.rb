3f0b753534d39ec712a5622ebd6fb575db522e77
EC2 Default User <ec2-user@ip-172-31-21-194.ap-northeast-1.compute.internal>
2022-08-11 09:42:23 +0000

    E6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
 
+  require "uri"
+
+  def description_url_to_link description
+
+    URI.extract(description, ['http','https']).uniq.each do |url|
+      sub_description = ""
+      sub_description << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      description.gsub!(url, sub_description)
+    end
+
+    return description
+  end
+  
+  
+  
+  
+
   def full_title(page_name = "")
     base_title = "Sample"
     if page_name.empty?
@@ -8,4 +26,4 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
-end
+end  
